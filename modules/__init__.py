19f1b93ef7fc6d5bfa8817fc20b121ff2e9d0d67
fallcorey <fallcorey.a@gmail.com>
2026-03-27 07:04:29 +0200

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,8 @@
+# NEXUS-OSINT Modules
+# OSINT modules for information gathering
+
+from . import username_osint
+from . import email_osint
+from . import ip_osint
+
+__all__ = ['username_osint', 'email_osint', 'ip_osint']
